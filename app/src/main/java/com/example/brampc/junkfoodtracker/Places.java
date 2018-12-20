053a579c1c1fad51cfda5bd9786f5547fd961f81
Unknown <timoo.dannis@gmail.com>
2018-12-20 19:41:10 +0100

    detailed layout
    jk;lfjsl

--- app/src/main/java/com/example/brampc/junkfoodtracker/Places.java
+++ app/src/main/java/com/example/brampc/junkfoodtracker/Places.java
@@ -4,22 +4,25 @@ import android.net.Uri;
 
 import com.google.android.gms.maps.model.LatLng;
 
+import java.io.Serializable;
 import java.util.List;
 
-public class Places {
+public class Places implements Serializable  {
     private String name;
     private String placeID;
     private String icon;
     private String Street;
-    private LatLng location;
     private float Rating;
+    private double latitude;
+    private double longditude;
 
     public Places(String name, String placeID, String icon, String street, LatLng location, float rating) {
         this.name = name;
         this.placeID = placeID;
         this.icon = icon;
         Street = street;
-        this.location = location;
+        latitude = location.latitude;
+        longditude = location.longitude;
         Rating = rating;
     }
 
@@ -40,11 +43,7 @@ public class Places {
     }
 
     public LatLng getLocation() {
-        return location;
-    }
-
-    public void setLocation(LatLng location) {
-        this.location = location;
+        return new LatLng(latitude, longditude);
     }
 
     public float getRating() {
--- app/src/main/java/com/example/brampc/junkfoodtracker/PlacesAdapter.java
+++ app/src/main/java/com/example/brampc/junkfoodtracker/PlacesAdapter.java
@@ -1,18 +1,21 @@
 package com.example.brampc.junkfoodtracker;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.google.android.gms.location.places.Place;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
 public class PlacesAdapter extends RecyclerView.Adapter<PlacesAdapter.Viewholder>{
@@ -46,10 +49,18 @@ public class PlacesAdapter extends RecyclerView.Adapter<PlacesAdapter.Viewholder
     @Override
     public void onBindViewHolder(Viewholder viewholder, final int i){
         Log.d(TAG, "onBlindViewHolder: called.");
-        Places hueLamp = allHueLamps.get(i);
+        final Places hueLamp = allHueLamps.get(i);
         viewholder.name.setText(hueLamp.getName());
         viewholder.street.setText(hueLamp.getStreet());
         viewholder.rating.setText(hueLamp.getRating()+ "");
+        viewholder.information.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, restaurant_informatioon.class);
+                intent.putExtra("hue", (Serializable) allHueLamps.get(i));
+                context.startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -61,6 +72,7 @@ public class PlacesAdapter extends RecyclerView.Adapter<PlacesAdapter.Viewholder
         TextView name;
         TextView street;
         TextView rating;
+        Button information;
         RelativeLayout parentLayout;
 
         public Viewholder(@NonNull View itemView) {
@@ -69,18 +81,7 @@ public class PlacesAdapter extends RecyclerView.Adapter<PlacesAdapter.Viewholder
             parentLayout = itemView.findViewById(R.id.main_list);
             street = itemView.findViewById(R.id.list_straat);
             rating = itemView.findViewById(R.id.list_Rating);
-
-            itemView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    if(mListener != null){
-                        int i = getAdapterPosition();
-                        if(i != RecyclerView.NO_POSITION) {
-                            mListener.onItemClick(i);
-                        }
-                    }
-                }
-            });
+            information = itemView.findViewById(R.id.information_button);
         }
     }
 }
