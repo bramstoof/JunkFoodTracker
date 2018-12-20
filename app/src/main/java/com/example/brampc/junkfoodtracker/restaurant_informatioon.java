d3b78ba5fc37895deadc9fca21e027c5b9bdd4e6
Unknown <timoo.dannis@gmail.com>
2018-12-20 19:54:44 +0100

    sdf
    sdf

--- app/src/main/java/com/example/brampc/junkfoodtracker/restaurant_informatioon.java
+++ app/src/main/java/com/example/brampc/junkfoodtracker/restaurant_informatioon.java
@@ -2,8 +2,10 @@ package com.example.brampc.junkfoodtracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class restaurant_informatioon extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
