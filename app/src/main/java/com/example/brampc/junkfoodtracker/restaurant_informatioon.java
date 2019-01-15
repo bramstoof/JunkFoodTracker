4bc581a094355cbb28c43d5ea1c5a55db3764c85
Unknown <timoo.dannis@gmail.com>
2019-01-15 16:36:29 +0100

    review fragment changes

--- app/src/main/java/com/example/brampc/junkfoodtracker/restaurant_informatioon.java
+++ app/src/main/java/com/example/brampc/junkfoodtracker/restaurant_informatioon.java
@@ -3,10 +3,13 @@ package com.example.brampc.junkfoodtracker;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class restaurant_informatioon extends AppCompatActivity {
+public class restaurant_informatioon extends AppCompatActivity  {
 
+    private Button writeReview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +20,7 @@ public class restaurant_informatioon extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_informatioon);
         Bundle extras = getIntent().getExtras();
+        writeReview = findViewById(R.id.Restaurant_writeReview);
         if(extras != null) {
             final Places hue = (Places) extras.getSerializable("hue");
             restaurantName = findViewById(R.id.restaurant_name);
@@ -28,6 +32,13 @@ public class restaurant_informatioon extends AppCompatActivity {
             float rating = hue.getRating();
             restaurantRating.setText(Float.toString(rating));
         }
+        writeReview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                review_fragment review_fragment = new review_fragment();
+                review_fragment.show(getSupportFragmentManager(),"Schrijf een review:");
+            }
+        });
 
     }
 }
